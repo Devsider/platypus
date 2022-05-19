eacb2083b4f7a5e56dafe7f1d184d39f060381a3
Techsider <54804321+Devsider@users.noreply.github.com>
2022-05-19 21:54:20 +0200

    Perry.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 let positions = [
-    [-0.1, 0, 30],
-    [1.1, 0.1, 50],
-    [-0.1, 0, 30],
-    [-0.1, 0, 30],
-    [-0.1, 0, 30],
+    [0, 0, 30],
+    [0, 0, 30],
+    [0, 0, 30],
+    [0, 0, 30],
+    [0, 0, 30],
 ];
 
 let timings = [
     0, 
-    1000,
-    400,
-    800,
-    1200
+    2000,
+    2000,
+    2000,
+    2000,
 ];
 
 let movingPlatypus = document.getElementById("moving-platypus");
@@ -22,9 +22,20 @@ movingPlatypus.style.transform = "rotate(" + positions[0][2] + "deg)";
 let totalTimeout = 0;
 for (let index = 0; index < timings.length; index++) {
     const element = timings[index];
+    console.log(`Timeout ${element}: ${totalTimeout}`);
     
     totalTimeout = totalTimeout + element;
     setTimeout(() => {
-        movingPlatypus.style.transform = "rotate(" + positions[i][2] + "deg)"; 
-    }, totalTimeOut);
+        movingPlatypus.style.transform = "rotate(" + positions[index][2] + "deg)"; 
+        movingPlatypus.style.top = "calc(100vh * " + positions[index][1] + ")";
+        movingPlatypus.style.left = "calc(100vw * " + positions[index][0] + ")";
+    }, totalTimeout);
 }
+
+/* 
+AUDIO
+*/
+
+let audio = new Audio("./assets/doo_bee_doo_bee_doo_bah.mp3");
+audio.play();
+audio.volume = 0.8;
