cba11596e5e595789a0d6fc4bee4e173615b28cb
Techsider <54804321+Devsider@users.noreply.github.com>
2022-05-21 02:29:35 +0200

    Step 3: ???

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 let positions = [
-    [100, 0, false, false, 30, false],
-    [0, 0, false, false, 30, false],
-    [0, 0, false, false, 30, true],
-    [0, 0, false, false, 30, false],
-    [0, 0, false, false, 30, false]
+    [0, 0, false, false, 45, false, 1],
+    [0, -120, false, true, -45, false, 1],
+    [0, 10, true, true, -45, true, 1],
+    [50, 0, true, false, 45, true, 1],
+    [-250, 200, true, false, -45, true, 1],
+    [260, 0, false, false, 45, true, 1],
+    [0, 200, false, false, 150, true, 1],
+    [0, -100, false, true, -120, true, 1],
+    [0, -100, true, true, -45, true, 1],
+    [-100, 200, true, false, -45, true, 2],
+    [-100, 200, true, false, -45, true, 1],
+    [0, 0, false, false, 45, false, 1],
+    [50, 0, false, false, 45, false, 1],
+    [70, 0, false, false, 45, false, 1]
 ];
 
 let timings = [
     0, 
-    2000,
-    2000,
-    2000,
-    2000,
+    1000,
+    1000,
+    1500,
+    1500,
+    1500,
+    1500,
+    1500,
+    1500,
+    1500,
+    400,
+    500,
+    150,
+    150
 ];
 
-let movingPlatypus = document.getElementById("moving-platypus");
 let movingPlatypusImg = document.getElementById("moving-platypus-img");
-
-movingPlatypus.style.transform = "rotate(" + positions[0][2] + "deg)";
-
+movingPlatypusImg.classList.remove("defaultLoc");
 
 let totalTimeout = 0;
 for (let index = 0; index < timings.length; index++) {
@@ -27,25 +42,30 @@ for (let index = 0; index < timings.length; index++) {
     
     totalTimeout = totalTimeout + element;
     setTimeout(() => {
-        movingPlatypus.style.transform = "rotate(" + positions[index][4] + "deg)"; 
+        console.log(`Moving Platypus: ${positions[index]}`);
 
+        movingPlatypusImg.style.transform = " translateX(-50%) translateY(-50%)";
         if(positions[index][5]){
-            movingPlatypus.style.transform += " scaleX(-1)";
+            movingPlatypusImg.style.transform += " scaleX(-1)";
         }
-
-        movingPlatypus.style.transform += " translateX(-50%) translateY(-50%)";
+        movingPlatypusImg.style.transform += "rotate(" + positions[index][4] + "deg)"; 
+        movingPlatypusImg.style.transform += " scale(" + positions[index][5] + ")";
 
         if(!positions[index][2]){
-            movingPlatypus.style.left = positions[index][0] + "px";
+            movingPlatypusImg.style.left = positions[index][0] + "px";
+            movingPlatypusImg.style.right = null;
         } else {
-            movingPlatypus.style.right = positions[index][0] + "px";
-        } 
+            movingPlatypusImg.style.left = null;
+            movingPlatypusImg.style.right = positions[index][0] + "px";
+        }
 
         if(!positions[index][3]){
-            movingPlatypus.style.top = positions[index][1] + "px";
+            movingPlatypusImg.style.top = positions[index][1] + "px";
+            movingPlatypusImg.style.bottom = null;
         } else {
-            movingPlatypus.style.bottom = positions[index][1] + "px";
-        } 
+            movingPlatypusImg.style.top = null;
+            movingPlatypusImg.style.bottom = positions[index][1] + "px";
+        }
     }, totalTimeout);
 }
 
@@ -55,5 +75,10 @@ AUDIO
 */
 
 let audio = new Audio("./assets/doo_bee_doo_bee_doo_bah.mp3");
-//audio.play();
-audio.volume = 0.8;
+//audio.muted = true;
+audio.volume = 0.3;
+audio.play();
+
+audio.onplay = function(){
+    console.log("Audio on play");
+}
